ceb1da15593eb71049a856abb730df9b6b9f9796
Kirill <kirrilf@mail.ru>
2019-03-19 01:14:47 +0300

    color dif

--- DeepFashion.py
+++ DeepFashion.py
@@ -8,4 +8,29 @@ im = cv2.imread("Clo1.jpg")
 
 resized = cv2.resize(im, (28, 28), interpolation = cv2.INTER_AREA)
 
-print(resized)
+#print(resized)
+
+#k = 0
+s1 = []
+
+for i in resized:
+	s = []
+	for j in i:
+		s.append(j[0])
+		s.append(j[1])
+		s.append(j[2])
+
+	s1.append(s)
+	#s.clear()
+	#tt = np.array(s)
+	##np.column_stack((a, b))
+	#print(s)
+	#pritn(tt)
+
+	#k+=1
+
+gg = np.array(s1)
+print(gg)
+
+
+	
--- TestFashion.py
+++ TestFashion.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-
+import cv2
 
 
 
@@ -18,7 +18,10 @@ loadedModel = tf.keras.models.model_from_json(loadedModelJson)
 
 loadedModel.load_weights("Fashion.h5")
 
+
+
 loadedModel.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
 
 
 
@@ -29,14 +32,46 @@ fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 
+#test1  = np.array([test_images[6]])
 
-test  = np.array([test_images[6]])
+
+#pred1 = loadedModel.predict(test1)
+
+
+#print("GG: ", np.argmax(pred1[0]))
+
+
+
+im = cv2.imread("8.jpg")
+
+resized = cv2.resize(im, (28, 28), interpolation = cv2.INTER_AREA)
+
+
+s1 = []
+
+for i in resized:
+	s = []
+	
+	for j in i:
+		#k = (j[0]+j[1]+j[2])/3
+		s.append(255-j[0])
+
+	s1.append(s)
+	
+
+gg = np.array(s1)
+#print(gg)
+
+test  = np.array([gg])
 
 
 pred = loadedModel.predict(test)
 
-print(np.argmax(pred[0]))
 
-print(test_images[0])
+print("TT: ", np.argmax(pred[0]))
 
+#print(test_images[0])
 
+#print("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
+
+#print(gg)
